9e1448e4f0d152dbe45ae5690547ee31fd7f6fcc
andrew cooke <andrew@acooke.org>
2013-12-01 11:50:57 -0300

    markup

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ What Else Should I Know?
     mystring = decrypt('password', ciphertext).decode('utf8')
 
 * (c) 2012 Andrew Cooke, andrew@acooke.org;
-  2013 [d10n](https://github.com/d10n), david@bitinvert.com.
+  2013 `d10n <https://github.com/d10n>`_, david@bitinvert.com.
   Released into the public domain for any use, but with absolutely no warranty.
     '''
 )
